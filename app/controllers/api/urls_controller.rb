69f7a1d4dc88cf13fe92a6d18479badf75083dc8
lbias <306508354@qq.com>
2017-08-06 22:44:05 +0800

    add stats query endpoint

--- app/controllers/api/urls_controller.rb
+++ app/controllers/api/urls_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Api
   class UrlsController < ApplicationController
     def index
@@ -13,5 +15,41 @@ module Api
     rescue ActiveRecord::RecordNotUnique
       @url = Url.find_by(url: params[:url])
     end
+
+    def unique_visitors_stat
+      url = Url.find_by(url: params[:url])
+      interval = params[:interval] || '1 hour'
+
+      # the idea of this query
+      # use generate_series to generate a series of time slots (start_time and end_time pair) based on interval
+      # use time_table to left join unique_visits based on time slot
+      if url
+        sql = "WITH time_table AS (
+                SELECT start_time, lead(start_time) OVER (ORDER BY start_time) AS end_time
+                FROM generate_series('#{params[:from]}'::timestamp,
+                                     '#{params[:to]}'::timestamp,
+                                     interval '#{interval}') AS start_time
+                )
+              SELECT start_time, count(unique_visits.visitor_uuid) AS unique_visits_count
+              FROM time_table
+                LEFT JOIN (select * from unique_visits where url_id = #{url.id}) as unique_visits
+                ON unique_visits.created_at >= time_table.start_time
+                  AND unique_visits.created_at <=  time_table.end_time
+              GROUP  BY start_time
+              ORDER  BY start_time"
+        interval_results = ActiveRecord::Base.connection.exec_query(sql)
+
+        render json: {
+          data: interval_results.rows,
+          meta: {
+            columns: interval_results.columns
+          }
+        }
+      else
+        render json: { error: 'url record not found' }, status: :not_found
+      end
+    rescue ArgumentError => ex
+      render json: { error: ex.messages }, status: :bad_request
+    end    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :urls, only: [:index, :show, :create]
+    resources :urls, only: [:index, :show, :create] do
+      collection do
+        get 'unique_visitors_stat'
+      end
+    end
   end
 
   get '/:short_url', to: 'app#redirect' # record request info and redirect
